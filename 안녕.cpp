0557431a1eeb2eb55fc9d3896bd35aa71a9ab726
chaboom <30657926+chaboom@users.noreply.github.com>
2017-09-19 13:38:55 +0900

    Update 안녕.cpp

--- 안녕.cpp
+++ 안녕.cpp
@@ -5,10 +5,10 @@ int Answer;
 int main(int argc, char** argv){
   int T, test_case;	
   cin >> T;	
-  for(test_case = 0; test_case  < T; test_case++)	{
+  for(test_case = 0; test_case  < T; test_case+	{
     Answer = 0;		
-    string input;		
-    cin >> input;		
+    string input;
+    cin >> input;
     int cnt_h = 0, cnt_e = 0, cnt_l = 0, cnt_o = 0;		
     for(int i =0; i < input.length(); i++){		   
       if(input[i] == 'h') cnt_h++;		    
